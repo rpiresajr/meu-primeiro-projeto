12777695c570f039d1675c6a51864f16152b658f
Renato <renato@MacBook-Pro-de-Renato.local>
2018-03-13 19:25:27 -0300

    Configurando o arquivo pow para JPA, MySQL, H2 e BCrypt

--- src/main/java/com/r2d2apps/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/r2d2apps/api/MeuPrimeiroProjetoApplication.java
@@ -1,14 +1,37 @@
 package com.r2d2apps.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.r2d2apps.api.utils.SenhaUtils;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtd_por_pagina;
 	
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
 		System.out.println("Meu primeiro projeto");
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner () {
+		return args -> {
+			System.out.println("Valor da variavel eh "+ this.qtd_por_pagina);
+			
+			String senha = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha: "+ senha);
+			
+			senha = SenhaUtils.gerarBCrypt("123456");
+			System.out.println("Senha: "+ senha);
+			
+			System.out.println("Validando a senha : " + SenhaUtils.senhaValida(senha, "123456"));
+			
+		};
 	}
 }
--- src/test/java/com/r2d2apps/api/MeuPrimeiroProjetoApplicationTests.java
+++ src/test/java/com/r2d2apps/api/MeuPrimeiroProjetoApplicationTests.java
@@ -1,16 +1,33 @@
 package com.r2d2apps.api;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class MeuPrimeiroProjetoApplicationTests {
-
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtd_por_pagina;
+	
+	
 	@Test
 	public void contextLoads() {
 	}
+	
+	@Test
+	public void TestCarregarContextoDeTeste() {
+		assertEquals(100, qtd_por_pagina);
+	}
+	
 
 }
